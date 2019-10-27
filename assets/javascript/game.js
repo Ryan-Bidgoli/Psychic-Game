2b12634bf12f83c2b5c3025ded19682625040dba
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-10-26 22:44:51 -0700

    Revamped and Rewritten files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,41 @@
-//Array of possible computer choices
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-//Variables for tracking players wins, losses, & guesses left
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 10; 
 var lettersGuessed = [];
-var computerGuess = [];
+var playerGuess = document.onkeyup;
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
+document.onkeyup = function (event) {
+  
+  playerGuess = event.key;
+ if(lettersGuessed.includes(playerGuess)) {
+    return;
+  }
+  //Previous guess is uploaded 
+  lettersGuessed.push(playerGuess);
+  if (computerGuess == playerGuess) {
+    wins++;
+    guessesLeft = 10;
+    guessesMade = [];
+  }
+  
+  if (computerGuess != playerGuess) {
+    guessesLeft--;
+    
+  }
 
-//Initial computer letter
-window.onload = function() {
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
+  if (guessesLeft == 0) {
+    losses++;
+    guessesLeft = 10;
+    guessesMade = [];
+  }
 
-
-//Game
-document.onkeyup = function(event) {
-	var playerGuess = event.key;
-	lettersGuessed.push(playerGuess);
-	console.log(computerGuess[0]);
-
-if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-	wins++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
-
-else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-	guessesLeft = guessesLeft-1;
-}
-
-else {
-	losses++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
-
-var html = "<p>Guess what letter I'm thinking of!</p>" +
-          "<p>Wins: " + wins + "</p>" +
-          "<p>Losses: " + losses + "</p>" +
-          "<p>Guesses left: " + guessesLeft + "</p>" +
-          "<p>Your guesses so far: " + lettersGuessed + "</p>";
- 
-document.querySelector("#game").innerHTML = html;
+  var html =
+    '<p>Guess what letter I am thinking of</p>' +
+    '<p>Wins: ' + wins + '</p>' +
+    '<p>Losses: ' + losses + '</p>' +
+    '<p>Guesses Left: ' + guessesLeft + '</p>' +
+    '<p>Your Guesses: ' + lettersGuessed + '</p>';
+document.getElementById("game").innerHTML = html;
 }
